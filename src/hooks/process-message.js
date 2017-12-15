9883cb256ce4ddcdf5391438f6989f93178a9cad
Peter.O <ojakaapeter@gmail.com>
2017-12-15 17:26:32 +0300

    Add user and date message was created

--- src/hooks/process-message.js
+++ src/hooks/process-message.js
@@ -5,6 +5,35 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function processMessage (hook) {
     // Hooks can either return nothing or a promise
     // that resolves with the `hook` object for asynchronous operations
+
+    // The authenticated user
+    const user = hook.params.user;
+
+    // The actual message text
+    const text = hook.data.text
+
+    // Messages can't be longer than 400 characters
+    .substring(0,400)
+
+    // Do some basic html escaping
+    .replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+
+    // Override the original data
+    hook.data = {
+      text,
+      
+      // Set the user id
+      userId: user._id,
+
+      // Add the current time via getTime
+      createdAt: new Date().getTime()
+    };
+
+    // Hooks can either return nothing or a promise
+    // that resolves with the 'hook' object for asynchronous operations
+    return Promise.resolve(hook);
+
+
     return Promise.resolve(hook);
   };
 };
